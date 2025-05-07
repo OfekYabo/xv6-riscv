1d76bb2e49a7fc5eebd521223577b97f3564f3a0
OfekYabo <yaboofe@post.bgu.ac.il>
2025-05-07 18:24:05 +0000

    ...

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -20,6 +20,7 @@
 #define SYS_link   19
 #define SYS_mkdir  20
 #define SYS_close  21
+// Peterson lock system call numbers
 #define SYS_peterson_create 22
 #define SYS_peterson_acquire 23
 #define SYS_peterson_release 24
